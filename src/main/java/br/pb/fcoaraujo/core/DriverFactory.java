d1d592e349c6cf92c8aff6311bc60f76f100ce3b
vandersonmgpb <vanderson1022@gmail.com>
2022-06-22 22:20:09 -0300

    Atualização no Curso de Selenium Driver, testes na tela de login site sr Barriga

--- src/main/java/br/pb/fcoaraujo/core/DriverFactory.java
+++ src/main/java/br/pb/fcoaraujo/core/DriverFactory.java
@@ -3,6 +3,7 @@ package br.pb.fcoaraujo.core;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
 	
@@ -12,7 +13,10 @@ public class DriverFactory {
 	
 	public static WebDriver getDriver(){
 		if(driver == null) {
-			driver = new ChromeDriver();
+			switch (Propriedades.browser){
+				case CHROME: driver = new ChromeDriver(); break;
+				case FIREFOX: driver = new FirefoxDriver(); break;
+			}
 			driver.manage().window().setSize(new Dimension(1200, 765));
 		}		
 		return driver;		
--- src/main/java/br/pb/fcoaraujo/core/Propriedades.java
+++ src/main/java/br/pb/fcoaraujo/core/Propriedades.java
@@ -3,5 +3,12 @@ package br.pb.fcoaraujo.core;
 public class Propriedades {
 	
 	public static boolean FECHAR_BROWSER = false;
+	
+	public static Browsers browser = Browsers.CHROME;
+	
+	public enum Browsers {
+		CHROME,
+		FIREFOX,
+	}
 
 }
